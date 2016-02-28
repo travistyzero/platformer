3bfc16a1a04139bbf543592f1ca7fc463d7b900c
Travis <mail@travisbloomfield.com>
2016-02-28 20:00:20 +1100

    Added Level class.

--- js/platformer.js
+++ js/platformer.js
@@ -16,3 +16,36 @@ var simpleLevelPlan = [
   "      xxxxxxxxxxxxxx  ",
   "                      "
 ];
+
+function Level(plan) {
+  this.width = plan[0].length;
+  this.height = plan.length;
+  this.grid = [];
+  this.actors = [];
+
+  for (var y = 0; y < this.height; y++) {
+    var line = plan[y],
+      gridLine = [];
+    for (var x = 0; x < this.width; x++) {
+      var ch = line[x],
+        fieldType = null;
+      var Actor = actorChars[ch];
+      if (Actor)
+        this.actors.push(new Actor(new Vector(x, y), ch));
+      else if (ch == "x")
+        fieldType = "wall";
+      else if (ch == "!")
+        fieldType = "lava";
+      gridLine.push(fieldType);
+    }
+    this.grid.push(gridLine);
+  }
+
+  this.player = this.actors.filter(function(actor) {
+    return actor.type == "player";
+  })[0];
+  this.status = this.finishDelay = null;
+}
+Level.prototype.isFinished = function() {
+  return this.status !== null && this.finishDelay < 0;
+};
