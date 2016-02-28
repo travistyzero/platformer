d3dbd6ec5099d04ff056dff5eceb681fd133994e
Travis <mail@travisbloomfield.com>
2016-02-28 19:50:04 +1100

    Created simple level plan.

---
+++ js/platformer.js
@@ -0,0 +1,18 @@
+/*
+@ - Player start location
+o - Coin
+= - Lava moves horizontally
+| - Lava moves vertically
+v - Lava that only moves down
+*/
+var simpleLevelPlan = [
+  "                      ",
+  "                      ",
+  "  x              = x  ",
+  "  x         o o    x  ",
+  "  x @      xxxxx   x  ",
+  "  xxxxx            x  ",
+  "      x!!!!!!!!!!!!x  ",
+  "      xxxxxxxxxxxxxx  ",
+  "                      "
+];
