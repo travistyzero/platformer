d6618f67f4c0e77258e9fbce3afda6247e10a45f
Travis <mail@travisbloomfield.com>
2016-02-29 16:35:14 +1100

    Added Coin class.

--- js/platformer.js
+++ js/platformer.js
@@ -92,3 +92,10 @@ function Lava(pos, ch) {
   }
   this.type = "lava";
 }
+
+function Coin(pos) {
+  this.basePos = this.pos = pos.plus(new Vector(0.2, 0.1));
+  this.size = new Vector(0.6, 0.6);
+  this.wobble = Math.random() * Math.PI * 2;
+  this.type = "coin";
+}
